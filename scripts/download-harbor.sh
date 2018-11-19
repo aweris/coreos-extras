2ec4d575620c41bbcaf31f9aef3be9a9ef5581fd
Ali AKÇA <ali.akca@altpro.com.tr>
2018-11-19 16:20:56 +0300

    python, docker-compose installer added vmware-harbor downloader added

---
+++ scripts/download-harbor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -uxe
+
+VERSION=v1.5.4
+PACKAGE="harbor-offline-installer-${VERSION}"
+EXTRA_PARAMS="--with-clair"
+
+# make directory
+mkdir -p /opt/bin
+
+cd /opt
+
+curl -sL https://storage.googleapis.com/harbor-releases/${PACKAGE}.tgz -o /opt
+
+tar -xzvf ${PACKAGE}.tgz && rm -rf ${PACKAGE}.tgz
---
+++ scripts/install-docker-compose.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -uxe
+
+VERSION=1.23.1
+PACKAGE=docker-compose-`uname -s`-`uname -m`
+
+# make directory
+mkdir -p /opt/bin
+
+curl -L https://github.com/docker/compose/releases/download/${VERSION}/${PACKAGE} -o /opt/bin/docker-compose
+
+chmod +x /opt/bin/docker-compose
